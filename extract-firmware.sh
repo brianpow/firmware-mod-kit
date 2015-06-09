bd4cfc715e45c3ff0ac75359ce5cfa475c6ffc48
Brian Pow <brianpow@gmail.com>
2015-06-09 11:05:15 +0800

    Default extract folder is "filename without extension", instead of "fmk". Number will be appended automatically if folder exists.

--- extract-firmware.sh
+++ extract-firmware.sh
@@ -6,7 +6,23 @@ IMG="${1}"
 DIR="${2}"
 
 if [ "${DIR}" = "" ]; then
-	DIR="fmk"
+	FULLFILENAME=$(basename "$IMG")
+	FILEEXTENSION="${FULLFILENAME##*.}"
+	FILENAME="${FULLFILENAME%.*}"
+	if [ "${FILENAME}" == "" ]; then
+    	DIR="fmk"
+	else
+		DIR="$FILENAME"
+	fi
+	if [ -f "${DIR}" ] || [ -d "${DIR}" ]; then
+		for I in {1..90000}; do
+			NEW_DIR="${DIR}_${I}"
+			if [ ! -f "$NEW_DIR" ] && [ ! -d "$NEW_DIR" ]; then
+				DIR="$NEW_DIR"
+				break
+			fi
+		done
+	fi
 fi
 
 # Need to extract file systems as ROOT
